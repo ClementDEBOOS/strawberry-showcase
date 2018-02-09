8d4485f2cfcfc38ca4660ede00c29490fb06544f
clement <clement.deboos@gmail.com>
2018-02-09 11:06:46 +0100

    Rootage
    Add root map in appmodule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
+  exports: [RouterModule ],
+
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true } // <-- debugging purposes only
+  )
   ],
   providers: [],
   bootstrap: [AppComponent]
